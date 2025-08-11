0a62d2f6be290711034b6c6f4f9994d7c3689370
Yarema <yiosipiv@gwmail.gwu.edu>
2025-08-11 16:44:07 -0700

    Add files via upload
    Format the facebook links file. Output a dictionary including all users and their connections via directed graph.

---
+++ formatDataset.py
@@ -0,0 +1,19 @@
+def create_edges():
+    # Create edges from dataset provided in the .txt file in the path below
+    # Optimized for facebook-links dataset
+    # Inside the function, variable "z" represents a timestamp when a connection was made made between users "x" and "y". Not all 
+    # users have timestamps available. Timestamps are not the goal of this project, so "z is ignored"
+    edges = {}
+    with open("myfilepath/facebook-links.txt", "r") as file:
+        for line in file:
+            x, y, z = line.split()
+            if x not in edges:
+                edges[x] = []
+                edges[x].append(y)
+            else:
+                edges[x].append(y)
+    file.close()
+    return edges
+
+if __name__ == '__main__':
+    create_edges()
